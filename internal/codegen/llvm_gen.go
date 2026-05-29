570783c0d4b0556241becd630f2f7c44b9c8cfb5
Konstantin <kostyaprozorovofficial@yandex.ru>
2026-05-29 16:25:08 +0300

    oberon_to_llvm_types_convertion_started

--- internal/codegen/llvm_gen.go
+++ internal/codegen/llvm_gen.go
@@ -1,8 +1,12 @@
 package codegen
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/K00nstantin/Compilers_project/internal/ast"
 	"github.com/llir/llvm/ir"
+	"github.com/llir/llvm/ir/types"
 	"github.com/llir/llvm/ir/value"
 )
 
@@ -52,4 +56,57 @@ func (g *Generator) lookup(name string) (value.Value, bool) {
 		}
 	}
 	return nil, false
+}
+
+func (g *Generator) oberonTypeToLLVM(t ast.TypeExpr) types.Type {
+	switch tt := t.(type) {
+	case *ast.NamedType:
+		switch tt.Name {
+		case "INTEGER":
+			return types.I32
+		case "BOOLEAN":
+			return types.I1
+		default:
+			return types.I32
+		}
+	case *ast.ArrayType:
+		if len(tt.Lengths) != 1 {
+			panic("multi dismentional arrays are not supported yet")
+		}
+		length := g.constExprToInt(tt.Lengths[0])
+		elemType := g.oberonTypeToLLVM(tt.Elem)
+		return types.NewArray(uint64(length), elemType)
+	case *ast.RecordType:
+		fields := make([]types.Type, len(tt.Fields))
+		for i, f := range tt.Fields {
+			fields[i] = g.oberonTypeToLLVM(f.Type)
+		}
+		return types.NewStruct(fields...)
+	case *ast.PointerType:
+		return types.NewPointer(g.oberonTypeToLLVM(tt.Target))
+	default:
+		panic(fmt.Sprintf("unknown type %T", t))
+	}
+}
+
+func (g *Generator) constExprToInt(e ast.Expr) int {
+	switch exp := e.(type) {
+	case *ast.NumberExpr:
+		val, _ := strconv.Atoi(exp.Text)
+		return val
+	case *ast.BinaryExpr:
+		left := g.constExprToInt(exp.Left)
+		right := g.constExprToInt(exp.Right)
+		switch exp.Op {
+		case "+":
+			return left + right
+		case "-":
+			return left - right
+		case "*":
+			return left * right
+		case "/":
+			return left / right
+		}
+	}
+	return 0
 }
